6f109e982b3afcc449529ff232ad2a81f631309e
Pruthvijit <Pruthnaik25@gmail.com>
2025-06-18 19:18:48 +0530

    add form validation

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,46 +1,76 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
+import { validate } from "../Utils/Validate";
 
-const Login  =()=>{
+const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMsg ,setErrorMsg] = useState(null)
 
-    const [isSignIn,setIsSignIn] = useState(true)
-    const changeSignIn = () =>{
-        setIsSignIn(!isSignIn)
-        console.log(isSignIn)
-    }
-    return(
-        <div className="">
-            <Header/>
-            <div className="flex flex-col items-center ">
-            <form className="absolute top-[25%]  bg-gray-900/80 w-4/12  text-white py-8 px-6 rounded-lg">
-                <h1 className="text-3xl font-bold capitalize">{isSignIn? "Sign in":"sign up"}</h1>
-               { !isSignIn&& <input
-                className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
-                type="text" placeholder="Full Name" />
-               }
-                <input
-                className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
-                type="text" placeholder="Email Address" />
-                <input type="text" placeholder="Password"
-                className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
-                />
-                <button
-                className="bg-red-800 w-full mt-6 py-2 px-6 rounded-lg capitalize cursor-pointer"
-                >
-                    {!isSignIn? "Sign up":"Sign In"}
-                </button>
+  const changeSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
 
-                <p className=" mt-6 cursor-pointer"
-                    onClick={changeSignIn}
-                >{isSignIn? "New to Netflix?Sign up now.":"Already have account. Sign In Now"}</p>
-              </form>
-              </div>
-           <img src="https://assets.nflxext.com/ffe/siteui/vlv3/7968847f-3da9-44b3-8bbb-13a46579881f/web/IN-en-20250609-TRIFECTA-perspective_32b70b51-20d4-46db-8a1a-3d5428be5f0e_small.jpg"
-            alt="background" />
 
-              
-            </div>
-    )
-}
+  const email = useRef()
+  const password = useRef()
+
+
+  const checkUserInfo = (e)=>{
+    e.preventDefault()
+    console.log(email.current.value)
+    console.log(password.current.value)
+  const massage =  validate(email.current.value,password.current.value)
+  console.log(massage)
+  setErrorMsg(massage)
+
+  }
+  return (
+    <div className="">
+      <Header />
+      <div className="flex flex-col items-center ">
+        <form className="absolute top-[25%]  bg-gray-900/80 w-4/12  text-white py-8 px-6 rounded-lg">
+          <h1 className="text-3xl font-bold capitalize">
+            {isSignIn ? "Sign in" : "sign up"}
+          </h1>
+          {!isSignIn && (
+            <input 
+              className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
+              type="text"
+              placeholder="Full Name"
+            />
+          )}
+          <input
+            ref={email}
+            className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+          ref={password}
+            type="password"
+            placeholder="Password"
+            className="bg-gray-700 w-full mt-6 py-2 px-6 rounded-lg"
+          />
+          <p className="text-red-700 py-3 px-2 font-bold">{errorMsg}</p>
+          <button 
+          onClick={checkUserInfo}
+          className="bg-red-800 w-full mt-6 py-2 px-6 rounded-lg capitalize cursor-pointer">
+            {!isSignIn ? "Sign up" : "Sign In"}
+          </button>
+
+          <p className=" mt-6 cursor-pointer" onClick={changeSignIn}>
+            {isSignIn
+              ? "New to Netflix?Sign up now."
+              : "Already have account. Sign In Now"}
+          </p>
+        </form>
+      </div>
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/7968847f-3da9-44b3-8bbb-13a46579881f/web/IN-en-20250609-TRIFECTA-perspective_32b70b51-20d4-46db-8a1a-3d5428be5f0e_small.jpg"
+        alt="background"
+      />
+    </div>
+  );
+};
 
 export default Login;
